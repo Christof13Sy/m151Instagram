1e8dc7e1fc63c96952520063c0765fa0147cee57
csy114909 <csy114909@iet-gibb.ch>
2019-06-04 11:38:02 +0200

    initial commit

--- instagram/config/routes.rb
+++ instagram/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  root 'pages#home'
+  devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show]
+
+  resources :posts, only: [:index, :show, :create, :destroy] do
+    resources :photos, only: [:create]
+  end
 end
